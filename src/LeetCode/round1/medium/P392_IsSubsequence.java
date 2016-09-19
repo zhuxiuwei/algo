d3a752d18810b7d78f87ad3405e1fcfdf97a87d0
zhuxiuwei <jlu_zxw@126.com>
2016-09-20 00:09:06 +0800

    add solutions

--- src/LeetCode/round1/medium/P392_IsSubsequence.java
+++ src/LeetCode/round1/medium/P392_IsSubsequence.java
@@ -16,13 +16,27 @@ Return false.
 Follow up: If there are lots of incoming S, say S1, S2, ... , Sk where k >= 1B, and you want to check one by one to see if T has its subsequence. In this scenario, how would you change your code?
  */
 public class P392_IsSubsequence {
-
+	
+	/**
+	 * greedy. AC: 63 ms
+	 */
 	public boolean isSubsequence(String s, String t) {
-		return false;
+		int i = 0, j =0;
+		while(i < s.length() && j < t.length()) {
+			if(s.charAt(i) == t.charAt(j)){
+				i ++;
+				j ++;
+			}else{
+				j ++;
+			}
+		}
+		return i == s.length();
     }
 	
 	public static void main(String[] args) {
-
+		P392_IsSubsequence p = new P392_IsSubsequence();
+		System.out.println(p.isSubsequence("abc", "ahbgdc"));
+		System.out.println(p.isSubsequence("axc", "ahbgdc"));
 	}
 
 }
