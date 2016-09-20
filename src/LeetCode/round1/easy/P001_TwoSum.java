1136bc9fa3543a28f623d1d5af10c83c50b2b292
Xiuwei Zhu <xiuzhu@microsoft.com>
2016-09-20 21:32:59 +0800

    test push

--- src/LeetCode/round1/easy/P001_TwoSum.java
+++ src/LeetCode/round1/easy/P001_TwoSum.java
@@ -14,7 +14,7 @@ Given nums = [2, 7, 11, 15], target = 9,
 
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
-UPDATE (2016/2/13):
+UPDATE (2016/2/13): 
 The return format had been changed to zero-based indices. Please read the above updated description carefully.
  *
  */
