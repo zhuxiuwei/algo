897a6578231fab65fffc73357ad3591e4d08605d
zhuxiuwei <jlu_zxw@126.com>
2016-11-14 08:39:12 +0800

    new files

---
+++ src/LeetCode/round1/medium/P039_CombinationSum.java
@@ -0,0 +1,32 @@
+package LeetCode.round1.medium;
+
+import java.util.List;
+
+/**
+ * 161013
+Given a set of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+The same repeated number may be chosen from C unlimited number of times.
+
+Note:
+All numbers (including target) will be positive integers.
+The solution set must not contain duplicate combinations.
+
+
+For example, given candidate set [2, 3, 6, 7] and target 7, 
+A solution set is: 
+[
+  [7],
+  [2, 2, 3]
+]
+ */
+public class P039_CombinationSum {
+
+	public List<List<Integer>> combinationSum(int[] candidates, int target) {
+		
+		return null;
+    }
+	
+	public static void main(String[] args) {
+
+	}
+}
