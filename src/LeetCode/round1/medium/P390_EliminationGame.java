b136dff4767de32c0b5ebfa5e39b760ab0af5b09
zhuxiuwei <jlu_zxw@126.com>
2016-12-18 23:33:10 +0800

    temp

---
+++ src/LeetCode/round1/medium/P390_EliminationGame.java
@@ -0,0 +1,28 @@
+package LeetCode.round1.medium;
+/**
+ * 161218
+There is a list of sorted integers from 1 to n. Starting from left to right, remove the first number and every other number afterward until you reach the end of the list.
+Repeat the previous step again, but this time from right to left, remove the right most number and every other number from the remaining numbers.
+We keep repeating the steps again, alternating left to right and right to left, until a single number remains.
+Find the last number that remains starting with a list of length n.
+
+Example:
+Input:
+n = 9,
+
+1 2 3 4 5 6 7 8 9	-> remove 1 2 5 7 9
+2 4 6 8		-> remove 8 4
+2 6		-> remove 2
+6
+
+Output:
+6
+ */
+public class P390_EliminationGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
