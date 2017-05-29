74c27a4e0160293472c00128968159d9a435e006
zhuxiuwei <jlu_zxw@126.com>
2017-05-29 20:30:46 +0800

    新增一个九章算法面经

--- src/study/interview/jiuzhang/!README.md
+++ src/study/interview/jiuzhang/!README.md
@@ -6,7 +6,8 @@
 [链表求和。要求O（n）的时间复杂度。](https://github.com/zhuxiuwei/algo/blob/master/src/lintcode/round1/P167_AddTwoNumbers.java). 很简单的题，结果弄错了好几次。状态不太好。  
 [二叉树两个节点的最近公共祖先](https://github.com/zhuxiuwei/algo/blob/master/src/lintcode/round1/P088_LowestCommonAncestor.java). 还是要注意如何退出递归调用问题、**有状态的类可能引入bug**的问题。   
 [在一个文件中，找出所有的Anagram](https://github.com/zhuxiuwei/algo/blob/master/src/LeetCode/round1/easy/P438_FindAllAnagramsInAString.java)我的[原始方法](https://github.com/zhuxiuwei/algo/blob/master/src/LeetCode/round1/easy/P438_FindAllAnagramsInAString.java#L81)超时了。可以用**滑动窗口**。思路绕，自己想不出来。  
+后面还有几道题目，暂时没时间看了。  
 
-
-
+[《Amazon HackerRank OA 面经》](http://www.jiuzhang.com/qa/748/)  
+题目1：Find first repeating letter in a string. 比如输入“abcba”, 返回“a”。 [代码]()  
 
---
+++ src/study/interview/jiuzhang/FindFirstRepeatingLetterInAString.java
@@ -0,0 +1,30 @@
+package study.interview.jiuzhang;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * [《Amazon HackerRank OA 面经》](http://www.jiuzhang.com/qa/748/) 170529  
+ */
+public class FindFirstRepeatingLetterInAString {
+	public static char find(String s){
+		if(s == null || s.length() == 0)
+			return ' ';
+		int minIdx = Integer.MAX_VALUE;
+		Map<Character, Integer> map = new HashMap<Character, Integer>();
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			if(map.containsKey(c)){	//a dup found
+				if(map.get(c) < minIdx)
+					minIdx = map.get(c);
+			}
+			else
+				map.put(c, i);
+		}
+		return minIdx == Integer.MAX_VALUE ? ' ': s.charAt(minIdx);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(find("abbcca"));	//a
+	}
+}
