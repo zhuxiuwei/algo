356748e37832f325bb6b358225e885396dadee90
zhuxiuwei <zhuxiuwei@meituan.com>
2024-08-02 20:00:30 +0800

    doing

--- src/LeetCode/round3/P139_WordBreak.java
+++ src/LeetCode/round3/P139_WordBreak.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Set;
 
 /**
+ * 240802
  * Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
  * Note that the same word in the dictionary may be reused multiple times in the segmentation.
  *
