39189dd594d7649734a2fa572c068efd57fff309
zhuxiuwei <zhuxiuwei@meituan.com>
2024-09-04 15:39:50 +0800

    2

--- src/LeetCode/round3/P131_PalindromePartitioning.java
+++ src/LeetCode/round3/P131_PalindromePartitioning.java
@@ -1,9 +1,10 @@
 package LeetCode.round3;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
- * 240902 medium
+ * 240904 medium
  * Given a string s, partition s such that every substring of the partition is a palindrome. Return all possible palindrome partitioning of s.
  * A palindrome is a string that reads the same forward and backward.
  *
@@ -20,8 +21,63 @@ import java.util.List;
  * s contains only lowercase English letters.
  */
 public class P131_PalindromePartitioning {
+    /**
+     * AC: 18ms Beats 6.40%. Memory 56.50MB Beats 80.62%
+     * bp问题，对这类问题有阴影所以想的时间比较久，动手比较晚。但真写起来还挺顺利。
+     */
     public List<List<String>> partition(String s) {
-        return null;
+        return helper(s.toCharArray(), 0);
+    }
+
+    /**
+     * 大体思路：
+     * 对每一个字符，从当前开始位置往后，判断是不是形成回文。
+     * 如果能形成回文，就对它后面的部分进行递归调用。
+     * 将当前形成的回文，放到地柜调用返回结果的头部。
+     */
+    private List<List<String>> helper(char[] chars, int start){
+        List<List<String>> res = new ArrayList<>();
+        if(start <= chars.length - 1) {
+            for (int i = start; i < chars.length; i++) {
+                if (isPalindrome(chars, start, i)) {
+                    List<List<String>> tmpRes = helper(chars, i + 1);
+                    String cur = getSubStrFromCharArray(chars, start, i);
+                    if(tmpRes.isEmpty()){
+                        List<String> newList = new ArrayList<>();
+                        newList.add(cur);
+                        res.add(newList);
+                    }else {
+                        for (List<String> partResList : tmpRes) {
+                            partResList.add(0, cur);
+                        }
+                    }
+                    res.addAll(tmpRes);  //！！！！少了这行会返回空结果！！！！
+                }
+            }
+        }
+        return res;
+    }
+
+    //判断是否是回文
+    private boolean isPalindrome(char[] chars, int start, int end){
+        while (start <= end){
+            char charS = chars[start];
+            char chatE = chars[end];
+            if(chatE != charS)
+                return false;
+            start ++;
+            end --;
+        }
+        return true;
+    }
+
+    //获取子字符串
+    private String getSubStrFromCharArray(char[] chars, int start, int end){
+        StringBuilder sb = new StringBuilder();
+        for (int i = start; i <= end; i++) {
+            sb.append(chars[i]);
+        }
+        return sb.toString();
     }
 
     public static void main(String[] args) {
---
+++ src/LeetCode/round3/P279_PerfectSquares.java
@@ -0,0 +1,72 @@
+package LeetCode.round3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 240904 medium
+ * Given an integer n, return the least number of perfect square numbers that sum to n.
+ * A perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself. For example, 1, 4, 9, and 16 are perfect squares while 3 and 11 are not.
+ *
+ * Example 1:
+ * Input: n = 12
+ * Output: 3
+ * Explanation: 12 = 4 + 4 + 4.
+ *
+ * Example 2:
+ * Input: n = 13
+ * Output: 2
+ * Explanation: 13 = 4 + 9.
+ *
+ * Constraints:
+ * 1 <= n <= 10000
+ */
+public class P279_PerfectSquares {
+
+    /**
+     * 自底向上的dp。 AC: 188ms Beats 13.31%, Memory 44.02MB Beats 27.92%
+     * 思路：
+     * n的结果记为f(n)。初始条件： f(0)=0, f(1) = 1。
+     * for i = 2 to n:
+     *   for each perfect square that <= i:
+     *     计算tmp = f(i - perfectSquare) + 1
+     *     f(i) = 上面过程中最小的一个tmp。
+     * 循环结束后，返回f(n)即可。
+     */
+    public int numSquares(int n) {
+        int[] res = new int[n + 1];
+        res[1] = 1;
+
+        //记录perfectSquares
+        List<Integer> perfectSquares = new ArrayList<>();
+        for (int i = 1; ; i++) {
+            int square = i * i;
+            if(square <= n) {
+                perfectSquares.add(square);
+            }else{
+                break;
+            }
+        }
+
+        return helper(res, n, perfectSquares);
+    }
+
+    private int helper(int[] res, int n, List<Integer> perfectSquares){
+        for (int i = 2; i < res.length; i++) {
+            int tmp = Integer.MAX_VALUE;
+            for (int square: perfectSquares){
+                if(square <= i) {
+                    tmp = Math.min(res[i - square] + 1, tmp);
+                }
+            }
+            res[i] = tmp;
+        }
+        return res[n];
+    }
+
+    public static void main(String[] args) {
+        P279_PerfectSquares p = new P279_PerfectSquares();
+        System.out.println(p.numSquares(12));   //3, 12 = 4 + 4 + 4
+        System.out.println(p.numSquares(13));   //2, 13 = 4 + 9
+    }
+}
